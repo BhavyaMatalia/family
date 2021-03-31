1b32457f0d6202475fd6db1e049d4e4c78241f64
Purvit Mehta <34183303+Purv123@users.noreply.github.com>
2021-04-01 00:51:37 +0530

    Update
    Update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-document.querySelector('.rnOuter').addEventListener('click', (e) => {
-	const helperText = document.querySelector('.helper-text');
-	helperText.style.animation = 'fade-out 1s ease-out 1 forwards';
-	const curtainParts = [...document.querySelectorAll('.rnInner')];
-	curtainParts.forEach((curtain) => {
-		curtain.style.transform = 'scaleX(0)';
-	});
+document.querySelector(".rnOuter").addEventListener("click", (e) => {
+  const helperText = document.querySelector(".helper-text");
+  helperText.style.animation = "fade-out 1s ease-out 1 forwards";
+  const curtainParts = [...document.querySelectorAll(".rnInner")];
+  curtainParts.forEach((curtain) => {
+    curtain.style.transform = "scaleX(0)";
+  });
+  setTimeout(function () {
+    window.location.href = "./flip/index.html";
+  }, 7000);
 });
+
+document.querySelector(".aoTable").addEventListener("click", (e) => {
+	window.location.href = "./flip/index.html"
+  });
+  
